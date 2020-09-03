21ed3ace65dddafe1ea18efc91fde2404be9cc47
Agresh Gupta <agreshgupta4@gmail.com>
2020-09-03 20:39:28 +0530

    update

--- Day 2: Operators.py
+++ Day 2: Operators.py
@@ -1,9 +1,10 @@
+#taking input from the user
 meal_cost = float(input())
 tip_percent = int(input())
 tax_percent = int(input())
-
+#calculating tip and tax
 tip = meal_cost * tip_percent / 100
 tax = meal_cost * tax_percent / 100
 total = meal_cost + tip + tax
-
+#printing out to the console
 print("The total meal cost is " + str(round(total)) + " dollars.")
