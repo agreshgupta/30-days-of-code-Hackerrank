f57e6eed417e343336f54808f94d15bd664301ec
Agresh Gupta <agreshgupta4@gmail.com>
2020-09-09 20:04:25 +0530

    Create Day 21: Generics.java

---
+++ Day 21: Generics.java
@@ -0,0 +1,46 @@
+/**
+* Welcome Today we are going to se java for this cause python does not support generics!
+* may in future versions of python it will allow as to do so
+**/
+import java.util.*;
+
+class Printer <T> {
+
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+    public static <T> void printArray(T[] array){
+        for(T e : array)
+    System.out.println(""+e);
+    }
+}
+
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
