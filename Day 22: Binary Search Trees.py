e59c5536781c4384c1f4887ffd0bfeba1cae68c9
Agresh Gupta <agreshgupta4@gmail.com>
2020-09-09 20:14:57 +0530

    Create Day 22: Binary Search Trees.py

---
+++ Day 22: Binary Search Trees.py
@@ -0,0 +1,42 @@
+#Life going to be interestig now!
+#Creating Node
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+#defining Tree
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        #Write your code here
+        if root:
+            left= 0
+            right=0
+            if root.left:
+                left= 1+ self.getHeight(root.left)
+            if root.right:
+                right = 1+ self.getHeight(root.right)
+            return max(left,right)
+        else:
+            return 0
+        #return (1+ max(self.getHeight(root.left)),self.getHeight(root.right))
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)   
